0786048017edf4e0c07576a8afa0b2eea423582c
hredoyTNext <rafiul.hredoy@technonext.com>
2023-09-20 10:58:57 +0600

    demo of multiple reducer and usinng middle wear

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const redux = require("redux");
+const reduxLogger =require('redux-logger')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const createStore = redux.createStore;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const combineReducers = redux.combineReducers;
+
+const applyMiddleware=redux.applyMiddleware
+
+const logger =reduxLogger.createLogger()
+
+
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICE = "BUY_ICE";
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
+
+function buyIce() {
+  return {
+    type: BUY_ICE,
+    info: " action",
+  };
+}
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceState = {
+  numOfIces: 15,
+};
+
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return { ...state, numOfCakes: state.numOfCakes - 1 };
+    default:
+      return state;
+  }
+};
+
+const iceReducer = (state = initialIceState, action) => {
+  switch (action.type) {
+    case BUY_ICE:
+      return { ...state, numOfIces: state.numOfIces - 1 };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  ice: iceReducer,
+});
+
+const store = createStore(rootReducer,applyMiddleware(logger));
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+{}  )
+
+
+store.dispatch(buyIce());
+store.dispatch(buyIce());
+store.dispatch(buyIce());
+store.dispatch(buyIce());
+store.dispatch(buyIce());
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+console.log(store.getState());
+
+store.dispatch(buyIce());
+store.dispatch(buyCake());
+// unsubscribe();
