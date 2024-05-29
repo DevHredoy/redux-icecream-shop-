02b620f94ec4c1996d9ab5039f951598a20baa22
DevHredoy <rafiul.hredoy@technonext.com>
2024-05-29 17:59:49 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -1,81 +1,19 @@
-const redux = require("redux");
-const reduxLogger =require('redux-logger')
-
-const createStore = redux.createStore;
-
-const combineReducers = redux.combineReducers;
-
-const applyMiddleware=redux.applyMiddleware
-
-const logger =reduxLogger.createLogger()
+const CAKE_ORDERED = "CAKE_ORDERED";
 
 
-const BUY_CAKE = "BUY_CAKE";
-const BUY_ICE = "BUY_ICE";
-function buyCake() {
-  return {
-    type: BUY_CAKE,
-    info: "First redux action",
-  };
+
+function actionCreator(){
+
+return{
+
+  type: CAKE_ORDERED,
+  quantity:1,
+
+
 }
 
-function buyIce() {
-  return {
-    type: BUY_ICE,
-    info: " action",
-  };
+
+
+
+
 }
-
-const initialCakeState = {
-  numOfCakes: 10,
-};
-
-const initialIceState = {
-  numOfIces: 15,
-};
-
-const cakeReducer = (state = initialCakeState, action) => {
-  switch (action.type) {
-    case BUY_CAKE:
-      return { ...state, numOfCakes: state.numOfCakes - 1 };
-    default:
-      return state;
-  }
-};
-
-const iceReducer = (state = initialIceState, action) => {
-  switch (action.type) {
-    case BUY_ICE:
-      return { ...state, numOfIces: state.numOfIces - 1 };
-    default:
-      return state;
-  }
-};
-
-const rootReducer = combineReducers({
-  cake: cakeReducer,
-  ice: iceReducer,
-});
-
-const store = createStore(rootReducer,applyMiddleware(logger));
-console.log("Initial state", store.getState());
-
-const unsubscribe = store.subscribe(() =>
-{}  )
-
-
-store.dispatch(buyIce());
-store.dispatch(buyIce());
-store.dispatch(buyIce());
-store.dispatch(buyIce());
-store.dispatch(buyIce());
-
-store.dispatch(buyCake());
-store.dispatch(buyCake());
-store.dispatch(buyCake());
-
-console.log(store.getState());
-
-store.dispatch(buyIce());
-store.dispatch(buyCake());
-// unsubscribe();
