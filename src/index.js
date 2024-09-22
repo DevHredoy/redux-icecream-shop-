76a5b038c472a07f97bc49f4fda53c1702d26d94
DevHredoy <rafiul.hredoy@technonext.com>
2024-09-22 12:47:15 +0600

    saved

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
+// import { legacy_createStore as createStore} from 'redux'
+
+const redux = require("redux");
+const createStore=redux.createStore
 const CAKE_ORDERED = "CAKE_ORDERED";
 
+const orderCake = () => {
+  const ORDER_CAKE = "ORDER_CAKE";
+
+  return {
+    type: ORDER_CAKE,
+    quantity: 1,
+  };
+};
+
+const initialState = {
+  numberOfCakes: 26,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return { ...state, numberOfCakes: state.numberOfCakes - 1 };
+
+    default:
+      return state;
+  }
+};
 
 
-function actionCreator(){
-
-return{
-
-  type: CAKE_ORDERED,
-  quantity:1,
-
-
-}
-
-
-
-
-
-}
