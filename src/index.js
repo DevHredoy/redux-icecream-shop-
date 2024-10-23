fc7dd3007c48a279a2f620d7ccd4c615f39972d2
DevHredoy <rafiul.hredoy@technonext.com>
2024-10-23 20:49:30 +0600

    next is vid no: 07

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
-// import { legacy_createStore as createStore} from 'redux'
+const CAKE_ORDERED="CAKE_ORDERED"
 
-const redux = require("redux");
-const createStore=redux.createStore
-const CAKE_ORDERED = "CAKE_ORDERED";
 
-const orderCake = () => {
-  const ORDER_CAKE = "ORDER_CAKE";
+// action creator which is returning and action
+//action is an object with a type property
+//an action creator is a function which returns an action
+function orderCake(){
+return{type:CAKE_ORDERED,
+  quantity:1,}
 
-  return {
-    type: ORDER_CAKE,
-    quantity: 1,
-  };
-};
+}
 
-const initialState = {
-  numberOfCakes: 26,
-};
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case CAKE_ORDERED:
-      return { ...state, numberOfCakes: state.numberOfCakes - 1 };
+// below is the state , an object
 
+const initialState={
+  numOfCakes:10,
+  anotherProperty:0,
+}
+
+//below is the reducer a function which acts like a shopkeeper in the cake shop
+//this function take the initial state and take the action, based on the action it performs the necessary initiative.
+
+const reducer=(state=initialState,action)=>{
+
+switch(action.type){
+
+case CAKE_ORDERED: 
+    return{
+      //below we are copying hte whole state object so that we can only change the desired state frm the object as there might be multiple states
+      ...state,
+      numOfCakes:state.numOfCakes-1
+
+    }
     default:
-      return state;
-  }
-};
+      return state
 
+}
+
+reducer(initialState,orderCake);
+
+
+
+}
 
