cfe41abf19ddd58a0d809faa6766d25d8cb341f4
DevHredoy <rafiul.hredoy@technonext.com>
2024-12-31 20:15:24 +0600

    updated in video 07 last half

--- src/index.js
+++ src/index.js
@@ -1,45 +1,63 @@
-const CAKE_ORDERED="CAKE_ORDERED"
+// below syntax we are using to use the library from redux
+// we are going to use this to create the redux store
 
+const redux = require("redux");
 
-// action creator which is returning and action
+const createStore = redux.createStore;
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+// action creator which is returning an action
 //action is an object with a type property
 //an action creator is a function which returns an action
-function orderCake(){
-return{type:CAKE_ORDERED,
-  quantity:1,}
-
+function orderCake() {
+  return { type: CAKE_ORDERED, quantity: 1 };
 }
-
 
 // below is the state , an object
 
-const initialState={
-  numOfCakes:10,
-  anotherProperty:0,
-}
+const initialState = {
+  numOfCakes: 10,
+  anotherProperty: 0,
+};
 
 //below is the reducer a function which acts like a shopkeeper in the cake shop
 //this function take the initial state and take the action, based on the action it performs the necessary initiative.
 
-const reducer=(state=initialState,action)=>{
-
-switch(action.type){
-
-case CAKE_ORDERED: 
-    return{
-      //below we are copying hte whole state object so that we can only change the desired state frm the object as there might be multiple states
-      ...state,
-      numOfCakes:state.numOfCakes-1
-
-    }
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        //below we are copying hte whole state object so that we can only change the desired state frm the object as there might be multiple states
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
     default:
-      return state
+      return state;
+  }
 
+  // redux store is somthing which brings actions and reducers together
 }
 
-reducer(initialState,orderCake);
+// below is the first responsibility
+const store=createStore(reducer)
+
+//below is second responsibility, exposing the getState method to show the state
+console.log('initial state:',store.getState())
 
 
 
-}
+// 4th responsibility
 
+const unsubscribe=store.subscribe(()=>console.log("updated state:",store.getState()))
+
+
+
+// 3rd responsibility
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+//5th responsibity
+
+console.log("unsubscribe:",unsubscribe())
