b68fd498664aa9f946dbd5876ccffd610bc92401
DevHredoy <rafiul.hredoy@technonext.com>
2025-01-02 11:41:41 +0600

    it did well when I implemented by myself

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_STOCKED = "CAKE_STOCKED";
 
 // action creator which is returning an action
 //action is an object with a type property
 //an action creator is a function which returns an action
 function orderCake() {
   return { type: CAKE_ORDERED, quantity: 1 };
+}
+
+//RESTOCKING CAKE:
+function stockCake() {
+  return { type: CAKE_STOCKED, quantity: 1 };
 }
 
 // below is the state , an object
@@ -21,13 +27,10 @@ const initialState = {
   anotherProperty: 0,
 };
 
-
-
 // below is the reducer a function which acts like a shopkeeper in the cake shop
 // this function take the initial state and take the action, based on the action it performs the necessary initiative.
 
-
-// you are teaching the reducer the what he can do in a given situation. like you are training the person(reducer). reducer smoothly reducing the amount here 
+// you are teaching the reducer the what he can do in a given situation. like you are training the person(reducer). reducer smoothly reducing the amount here
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
@@ -35,6 +38,13 @@ const reducer = (state = initialState, action) => {
         //below we are copying hte whole state object so that we can only change the desired state frm the object as there might be multiple states
         ...state,
         numOfCakes: state.numOfCakes - 1,
+      };
+
+    case CAKE_STOCKED:
+      return {
+        //below we are copying hte whole state object so that we can only change the desired state frm the object as there might be multiple states
+        ...state,
+        numOfCakes: state.numOfCakes + 1,
       };
     default:
       return state;
@@ -62,18 +72,15 @@ store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
-// now what is happening in the above:When I call store.dispatch(orderCake()), 
-// the orderCake() function returns the type of 
-// the action (CAKE_ORDERED). This action is then passed 
-// to the reducer through the redux store, which updates the store by reducing the 
+store.dispatch(stockCake());
+// now what is happening in the above:When I call store.dispatch(orderCake()),
+// the orderCake() function returns the type of
+// the action (CAKE_ORDERED). This action is then passed
+// to the reducer through the redux store, which updates the store by reducing the
 // amount of cakes (numOfCakes) based on the logic defined in the reducer.
-
-
 
 //5th responsibity
 
 unsubscribe();
 
 store.dispatch(orderCake());
-
-
